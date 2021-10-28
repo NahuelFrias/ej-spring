10af4339b1cef1c45a96725ce7531dbde5113360
leonahuelfrias <leonahuelfrias@gmail.com>
2021-10-28 10:42:53 -0300

    clase controlador movida a carpeta controlador

--- src/main/java/com/ejemplospring/ejspring/controladores/PortalControlador.java
+++ src/main/java/com/ejemplospring/ejspring/controladores/PortalControlador.java
@@ -1,4 +1,4 @@
-package com.ejemplospring.ejspring;
+package com.ejemplospring.ejspring.controladores;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
